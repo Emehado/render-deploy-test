0f2384a51b4a4ab52fe5cfec5fcc0b1c33b3457d
Bright Larson Nanevie <emehado@gmail.com>
2026-01-08 11:35:12 +0000

    testing pr previews

--- apps/client/app/welcome/welcome.tsx
+++ apps/client/app/welcome/welcome.tsx
@@ -1,3 +1,7 @@
 export function Welcome() {
-  return <main className="flex items-center justify-center pt-16 pb-4"></main>;
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <h1 className="text-2xl font-bold">PR Preview Test</h1>
+    </main>
+  );
 }
