53a83ed04a74a8efa645432e99fe72226bdd1ca5
Bright Larson Nanevie <emehado@gmail.com>
2026-01-09 12:05:11 +0000

    test staging deployment on render

--- apps/client/app/welcome/welcome.tsx
+++ apps/client/app/welcome/welcome.tsx
@@ -1,7 +1,7 @@
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <h1 className="text-2xl font-bold">PR Preview Test</h1>
+      <h1 className="text-2xl font-bold">PR Preview Test for staging only</h1>
     </main>
   );
 }
