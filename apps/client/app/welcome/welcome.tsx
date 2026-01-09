36df04beba3b671513e187ea44f7166e9974a3e0
Bright Larson Nanevie <emehado@gmail.com>
2026-01-09 12:08:40 +0000

    testing pr previews again

--- apps/client/app/welcome/welcome.tsx
+++ apps/client/app/welcome/welcome.tsx
@@ -1,7 +1,9 @@
 export function Welcome() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <h1 className="text-2xl font-bold">PR Preview Test</h1>
+      <h1 className="text-2xl font-bold">
+        PR Preview Test again after staging environment
+      </h1>
     </main>
   );
 }
